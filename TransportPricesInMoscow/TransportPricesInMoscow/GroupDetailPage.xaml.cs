1df10bae131ee579151184056902ce0fa8a1ec89
Anton Lenev <m0rg0t.Anton@gmail.com>
2013-07-31 18:07:37 +0400

    Privacy policy

--- TransportPricesInMoscow/TransportPricesInMoscow/GroupDetailPage.xaml.cs
+++ TransportPricesInMoscow/TransportPricesInMoscow/GroupDetailPage.xaml.cs
@@ -13,6 +13,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.ApplicationSettings;
+using Callisto.Controls;
+using TransportPricesInMoscow.Controls;
 
 // Шаблон элемента страницы сведений о группе задокументирован по адресу http://go.microsoft.com/fwlink/?LinkId=234229
 
@@ -40,10 +43,55 @@ namespace TransportPricesInMoscow
         /// сеанса. Это значение будет равно NULL при первом посещении страницы.</param>
         protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
         {
-            // TODO: Создание соответствующей модели данных для области проблемы, чтобы заменить пример данных
-            var group = SampleDataSource.GetGroup((String)navigationParameter);
-            this.DefaultViewModel["Group"] = group;
-            this.DefaultViewModel["Items"] = group.Items;
+            try
+            {
+                // TODO: Создание соответствующей модели данных для области проблемы, чтобы заменить пример данных
+                var group = SampleDataSource.GetGroup((String)navigationParameter);
+                this.DefaultViewModel["Group"] = group;
+                this.DefaultViewModel["Items"] = group.Items;
+            }
+            catch { };
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            SettingsPane.GetForCurrentView().CommandsRequested -= Settings_CommandsRequested;
+            base.OnNavigatedFrom(e);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            SettingsPane.GetForCurrentView().CommandsRequested += Settings_CommandsRequested;
+            base.OnNavigatedTo(e);
+        }
+
+        void Settings_CommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
+        {
+            try
+            {
+                var viewAboutPage = new SettingsCommand("", "Об авторе", cmd =>
+                {
+                    //(Window.Current.Content as Frame).Navigate(typeof(AboutPage));
+                    var settingsFlyout = new SettingsFlyout();
+                    settingsFlyout.Content = new About();
+                    settingsFlyout.HeaderText = "Об авторе";
+
+                    settingsFlyout.IsOpen = true;
+                });
+                args.Request.ApplicationCommands.Add(viewAboutPage);
+
+                var privacypolicy = new SettingsCommand("", "Политика конфиденциальности", cmd =>
+                {
+                    var settingsFlyout = new SettingsFlyout();
+                    settingsFlyout.Content = new Privacy();
+                    settingsFlyout.HeaderText = "Политика конфиденциальности";
+
+                    settingsFlyout.IsOpen = true;
+                });
+                args.Request.ApplicationCommands.Add(privacypolicy);
+
+            }
+            catch { };
         }
 
         /// <summary>
